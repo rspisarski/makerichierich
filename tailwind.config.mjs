c6e678b99b15dfd2a8714c8696c397c6ef67c5a3
rspisarski <rspisarski@gmail.com>
2023-10-11 17:32:38 -0400

    Initial Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+	    container: {
+			center: true,
+			padding: {
+				DEFAULT: '2rem',
+				sm: '2rem',
+				lg: '4rem',
+				xl: '16rem',
+			},
+		},
+		extend: {
+			fontFamily: {
+        sans: ["Young Serif", "sans"],
+		},
+		},
+	},
+	plugins: [require('@tailwindcss/typography'),],
+}
